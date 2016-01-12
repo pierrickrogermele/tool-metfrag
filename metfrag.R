06b9e368f26715dd89aa14018eb63847da190361
Pierrick Roger Mele <pierrick.roger@gmail.com>
2016-01-12 16:18:24 +0100

    Correct bug in path when calling metfrag.

--- metfrag.R
+++ metfrag.R
@@ -1,9 +1,12 @@
-source(file.path(dirname(SCRIPT.PATH), '..', 'r-biodb', 'ChemSpiderConn.R'), chdir = TRUE)
-source(file.path(dirname(SCRIPT.PATH), '..', 'r-biodb', 'PubchemConn.R'), chdir = TRUE)
+source(file.path('..', 'r-lib', 'htmlhlp.R'), chdir = TRUE)
+source(file.path('..', 'r-biodb', 'ChemSpiderConn.R'), chdir = TRUE)
+source(file.path('..', 'r-biodb', 'PubchemConn.R'), chdir = TRUE)
 
 #############
 # CONSTANTS #
 #############
+
+CURRENT.FILE.PATH <- parent.frame(2)$ofile
 
 CHEMSPIDER <- 'chemspider'
 PUBCHEM <- 'pubchem'
@@ -16,7 +19,7 @@ DBS <- c(CHEMSPIDER, PUBCHEM)
 run.metfrag <- function(db, ms.mode, precursor.ionized.mass, relative.mass.deviation, formula, spectrum.file, output.param.file, output.file, html.output.file) {
 	
 	# Run metfrag
-	cmd  <- c(file.path(dirname(SCRIPT.PATH), '..', 'tool-metfrag', 'metfrag'),	'--spectrum', spectrum.file, '--mode', ms.mode, '--db', db, '-k', '744de16c-481a-489d-8803-48e9d35e7440', '--precursor-ionized-mass', precursor.ionized.mass, '-o', output.file, '--output-param-file', output.param.file, '--no-filter')
+	cmd  <- c(file.path(dirname(CURRENT.FILE.PATH), 'metfrag'),	'--spectrum', spectrum.file, '--mode', ms.mode, '--db', db, '-k', '744de16c-481a-489d-8803-48e9d35e7440', '--precursor-ionized-mass', precursor.ionized.mass, '-o', output.file, '--output-param-file', output.param.file, '--no-filter')
 	cmd <- c(cmd, if (is.null(formula)) c('--relative-mass-deviation', relative.mass.deviation) else c('--precursor-formula', formula))
 	system(paste(cmd, collapse = ' '))
 
