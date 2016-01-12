2abb0e9399b806e35f26d31a056d425f4afa937d
Pierrick Roger Mele <pierrick.roger@gmail.com>
2016-01-12 13:04:35 +0100

    Import html output function.

--- metfrag.R
+++ metfrag.R
@@ -13,7 +13,7 @@ DBS <- c(CHEMSPIDER, PUBCHEM)
 # RUN METFRAG #
 ###############
 
-run.metfrag <- function(db, ms.mode, precursor.ionized.mass, relative.mass.deviation, formula, spectrum.file, output.param.file, output.file) {
+run.metfrag <- function(db, ms.mode, precursor.ionized.mass, relative.mass.deviation, formula, spectrum.file, output.param.file, output.file, html.output.file) {
 	
 	# Run metfrag
 	cmd  <- c(file.path(dirname(SCRIPT.PATH), '..', 'tool-metfrag', 'metfrag'),	'--spectrum', spectrum.file, '--mode', ms.mode, '--db', db, '-k', '744de16c-481a-489d-8803-48e9d35e7440', '--precursor-ionized-mass', precursor.ionized.mass, '-o', output.file, '--output-param-file', output.param.file, '--no-filter')
@@ -24,4 +24,47 @@ run.metfrag <- function(db, ms.mode, precursor.ionized.mass, relative.mass.devia
 	results <- read.table(output.file, sep='|', header = TRUE, comment.char = "", stringsAsFactors = FALSE, quote = "")
 
 	return(results)
+}
+
+#############################
+# WRITE METFRAG HTML OUTPUT #
+#############################
+
+write.metfrag.html.output <- function(results, db, file, title = NULL, highlighted.lines = NULL) {
+
+	# Set molecule name when absent
+	if (db == PUBCHEM) {
+		conn <- switch(db, pubchem = PubchemConn$new(useragent = USERAGENT))
+		noname <- is.na(results[['IUPACName']])
+		results[noname, 'IUPACName'] <- vapply(results[noname, 'Identifier'], function(id) { e <- conn$getEntry(id) ; if (is.null(e)) NA_character_ else e$getName() }, FUN.VALUE = '')
+	}
+
+	# Add Structure column and set inside a link to molecule image
+	results[['Structure']] <- vapply(results[['Identifier']], function(id) paste0('<img src="', switch(db, pubchem = get.pubchem.image.url(id), chemspider = get.chemspider.image.url(id)), '" width="150" height="150"/>'), FUN.VALUE = '')
+
+	# Add links to db page
+	results[['Identifier']] <- vapply(results[['Identifier']], function(id) paste0('<a href="', switch(db, pubchem = get.pubchem.entry.url(id), chemspider = get.chemspider.entry.url(id)), '">', id, '</a>'), FUN.VALUE = '')
+
+	# Write HTML
+	html <- HtmlWriter(file = file)
+	html$writeBegTag('html')
+	html$writeBegTag('header')
+	html$writeTag('title', text = title)
+	html$writeBegTag('style')
+	html$write('table, th, td { border-collapse: collapse; }')
+	html$write('table, th { border: 1px solid black; }')
+	html$write('td { border-left: 1px solid black; border-right: 1px solid black; }')
+	html$write('th, td { padding: 5px; }')
+	html$write('th { background-color: LightBlue; }')
+	html$write('tr:nth-child(even) { background-color: LemonChiffon; }')
+	html$write('tr:nth-child(odd) { background-color: LightGreen; }')
+	for (n in highlighted.lines)
+		html$write(paste0('tr:nth-child(', n + 1, ') { background-color: Tomato; }'))
+	html$writeEndTag('style')
+	html$writeEndTag('header')
+	html$writeBegTag('body')
+	html$writeTag('h1', text = title)
+	html$writeTable(results)
+	html$writeEndTag('body')
+	html$writeEndTag('html')
 }
